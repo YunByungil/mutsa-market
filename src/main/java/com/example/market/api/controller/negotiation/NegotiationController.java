b7dc62b6fa76fd953224891dc12cf82fd42a63df
YunByungil <dbsquddlfz@naver.com>
2023-11-08 01:34:40 +0900

    :recycle: ApiResponse, NegotiationResponse 적용

--- src/main/java/com/example/market/api/controller/negotiation/NegotiationController.java
+++ src/main/java/com/example/market/api/controller/negotiation/NegotiationController.java
@@ -1,8 +1,10 @@
 package com.example.market.api.controller.negotiation;
 
+import com.example.market.api.ApiResponse;
 import com.example.market.dto.negotiation.request.NegotiationCreateRequestDto;
 import com.example.market.dto.negotiation.request.NegotiationUpdateRequestDto;
 import com.example.market.dto.negotiation.response.NegotiationListResponseDto;
+import com.example.market.dto.negotiation.response.NegotiationResponse;
 import com.example.market.dto.negotiation.response.NegotiationResponseDto;
 import com.example.market.service.NegotiationService;
 import jakarta.validation.Valid;
@@ -22,41 +24,53 @@ public class NegotiationController {
     private final NegotiationService negotiationService;
 
     @PostMapping("/items/{itemId}/proposals")
-    public NegotiationResponseDto createNegotiation(@PathVariable Long itemId,
-                                                    @Valid  @RequestBody NegotiationCreateRequestDto createDto,
-                                                    Authentication authentication) {
+    public ApiResponse<NegotiationResponse> createNegotiation(@PathVariable Long itemId,
+                                                              @Valid @RequestBody NegotiationCreateRequestDto createDto,
+                                                              Authentication authentication) {
         Long userId = Long.parseLong(authentication.getName());
-        negotiationService.createNegotiation(itemId, createDto, userId);
 
-        return new NegotiationResponseDto(REGISTER_NEGOTIATION);
+        return ApiResponse.ok(negotiationService.createNegotiation(itemId, createDto, userId));
     }
 
-    @GetMapping("/items/{itemId}/proposals")
-    public Page<NegotiationListResponseDto> readAllNegotiation(@PathVariable Long itemId,
-                                                               @RequestParam(value = "page", defaultValue = "0") int page,
-                                                               @RequestParam(value = "limit", defaultValue = "20") int limit,
-                                                               Authentication authentication) {
+//    @GetMapping("/items/{itemId}/proposals")
+//    public ApiResponse<Page<NegotiationListResponseDto>> readAllNegotiation(@PathVariable Long itemId,
+//                                                               @RequestParam(value = "page", defaultValue = "0") int page,
+//                                                               @RequestParam(value = "limit", defaultValue = "20") int limit,
+//                                                               Authentication authentication) {
+//        Long userId = Long.parseLong(authentication.getName());
+//        return ApiResponse.ok(negotiationService.readAllNegotiation(itemId, page, limit, userId));
+//    }
+
+    @GetMapping("/items/received/proposals")
+    public ApiResponse<Page<NegotiationResponse>> getReceivedNegotiationItems(final Authentication authentication,
+                                                                              @RequestParam(value = "page", defaultValue = "1") int page) {
         Long userId = Long.parseLong(authentication.getName());
-        return negotiationService.readAllNegotiation(itemId, page, limit, userId);
+        return ApiResponse.ok(negotiationService.getReceivedNegotiationItems(userId, page));
     }
 
-    @PutMapping("/items/{itemId}/proposals/{proposalId}")
-    public NegotiationResponseDto updateNegotiation(@PathVariable Long itemId,
-                                                    @PathVariable Long proposalId,
-                                                    @Valid @RequestBody NegotiationUpdateRequestDto updateDto,
-                                                    Authentication authentication) {
+    @GetMapping("/items/sent/proposals")
+    public ApiResponse<Page<NegotiationResponse>> getSentNegotiationItems(final Authentication authentication,
+                                                                          @RequestParam(value = "page", defaultValue = "1") int page) {
         Long userId = Long.parseLong(authentication.getName());
-        return negotiationService.updateNegotiation(itemId, proposalId, updateDto, userId);
-
+        return ApiResponse.ok(negotiationService.getSentNegotiationItems(userId, page));
     }
 
-    @DeleteMapping("/items/{itemId}/proposals/{proposalId}")
-    public NegotiationResponseDto deleteNegotiation(@PathVariable Long itemId,
-                                                    @PathVariable Long proposalId,
-                                                    Authentication authentication) {
-        Long userId = Long.parseLong(authentication.getName());
-        negotiationService.deleteNegotiation(itemId, proposalId, userId);
+//    @PutMapping("/items/{itemId}/proposals/{proposalId}")
+//    public NegotiationResponseDto updateNegotiation(@PathVariable Long itemId,
+//                                                    @PathVariable Long proposalId,
+//                                                    @Valid @RequestBody NegotiationUpdateRequestDto updateDto,
+//                                                    Authentication authentication) {
+//        Long userId = Long.parseLong(authentication.getName());
+//        return negotiationService.updateNegotiation(itemId, proposalId, updateDto, userId);
+//    }
 
-        return new NegotiationResponseDto(DELETE_NEGOTIATION);
-    }
+//    @DeleteMapping("/items/{itemId}/proposals/{proposalId}")
+//    public NegotiationResponseDto deleteNegotiation(@PathVariable Long itemId,
+//                                                    @PathVariable Long proposalId,
+//                                                    Authentication authentication) {
+//        Long userId = Long.parseLong(authentication.getName());
+//        negotiationService.deleteNegotiation(itemId, proposalId, userId);
+//
+//        return new NegotiationResponseDto(DELETE_NEGOTIATION);
+//    }
 }
