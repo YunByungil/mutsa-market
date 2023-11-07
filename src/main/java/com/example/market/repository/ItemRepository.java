cb21cd00b70899c586fd3fd9a9d6ba0456414b5c
YunByungil <dbsquddlfz@naver.com>
2023-11-07 18:17:56 +0900

    :sparkles: ItemRepository# 판매 중인 상품만 검색 기능 추가

--- src/main/java/com/example/market/repository/ItemRepository.java
+++ src/main/java/com/example/market/repository/ItemRepository.java
@@ -1,7 +1,14 @@
 package com.example.market.repository;
 
 import com.example.market.domain.entity.Item;
+import com.example.market.domain.entity.enums.ItemStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    Page<Item> findAllByStatus(ItemStatus status, Pageable pageable);
 }
