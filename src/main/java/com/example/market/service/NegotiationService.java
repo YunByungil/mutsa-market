5521e0c979c4a965c3a0803a9463ad4439412719
YunByungil <dbsquddlfz@naver.com>
2023-11-08 01:37:03 +0900

    :recycle: itemId 기준으로 가격 제안 내역 불러오던 로직을 api 분리하여 메서드 2개로 나눔

--- src/main/java/com/example/market/service/NegotiationService.java
+++ src/main/java/com/example/market/service/NegotiationService.java
@@ -7,6 +7,7 @@ import com.example.market.domain.entity.enums.NegotiationStatus;
 import com.example.market.domain.entity.user.User;
 import com.example.market.dto.negotiation.request.*;
 import com.example.market.dto.negotiation.response.NegotiationListResponseDto;
+import com.example.market.dto.negotiation.response.NegotiationResponse;
 import com.example.market.dto.negotiation.response.NegotiationResponseDto;
 import com.example.market.exception.MarketAppException;
 import com.example.market.repository.ItemRepository;
@@ -35,129 +36,47 @@ public class NegotiationService {
     private final UserRepository userRepository;
 
     @Transactional
-    public Long createNegotiation(Long itemId, NegotiationCreateRequestDto dto, Long userId) {
+    public NegotiationResponse createNegotiation(Long itemId, NegotiationCreateRequestDto dto, Long userId) {
         Item item = itemRepository.findById(itemId)
                 .orElseThrow(() -> new MarketAppException(NOT_FOUND_ITEM, NOT_FOUND_ITEM.getMessage()));
-        User user = userRepository.findById(userId)
+        User buyer = userRepository.findById(userId)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 
-        return negotiationRepository.save(dto.toEntity(item, user)).getId();
+        Negotiation negotiation = negotiationRepository.save(dto.toEntity(item, buyer, item.getUser()));
+        // TODO: 중복 신청 X 로직 추가
+
+        return NegotiationResponse.of(negotiation);
     }
 
-    public Page<NegotiationListResponseDto> readAllNegotiation(Long itemId, int page, int limit, Long userId) {
-        Item item = itemRepository.findById(itemId)
-                .orElseThrow(() -> new MarketAppException(NOT_FOUND_ITEM, NOT_FOUND_ITEM.getMessage()));
-
-        Pageable pageable = PageRequest.of(page, limit, Sort.by("id").ascending());
-
+    public Page<NegotiationResponse> getReceivedNegotiationItems(final Long userId, final int page) {
         User user = userRepository.findById(userId)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+                .orElseThrow(() -> new MarketAppException(NOT_FOUND_USER, NOT_FOUND_USER.getMessage()));
+        Page<Negotiation> negotiations = negotiationRepository.
+                findAllBySellerId(userId, PageRequest.of(page - 1, 20, Sort.by("id").ascending()));
 
-        // 1. 아이템 주인일 때 그 아이템에 대한 네고 정보를 다 불러와야 함.
-        if (item.getUser().getId() == user.getId()) {
-            return getNegotiationListResponseDtoBySeller(itemId, pageable);
-        }
-
-        // 2. 주인이 아니기 때문에 Buyer 기준
-        return getNegotiationListResponseDtoByBuyer(itemId, userId, pageable);
+        return negotiations.map(NegotiationResponse::of);
     }
 
-    @Transactional
-    public NegotiationResponseDto updateNegotiation(Long itemId, Long negotiationId, NegotiationUpdateRequestDto updateDto, Long userId) {
-        Item item = itemRepository.findById(itemId)
-                .orElseThrow(() -> new MarketAppException(NOT_FOUND_ITEM, NOT_FOUND_ITEM.getMessage()));
-
-        Negotiation negotiation = negotiationRepository.findById(negotiationId)
-                .orElseThrow(() -> new MarketAppException(NOT_FOUND_NEGOTIATION, NOT_FOUND_NEGOTIATION.getMessage()));
-
+    public Page<NegotiationResponse> getSentNegotiationItems(final Long userId, final int page) {
         User user = userRepository.findById(userId)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+                .orElseThrow(() -> new MarketAppException(NOT_FOUND_USER, NOT_FOUND_USER.getMessage()));
+        Page<Negotiation> negotiations = negotiationRepository.
+                findAllBySellerId(userId, PageRequest.of(page - 1, 20, Sort.by("id").ascending()));
 
-        NegotiationStatus status = null;
-        if (updateDto.getSuggestedPrice() == 0) {
-            status = NegotiationStatus.findByNegotiationStatus(updateDto.getStatus());
-        }
-
-        if (item.getUser().getId() == user.getId()) {
-            negotiation.updateNegotiationStatus(status);
-            return new NegotiationResponseDto(CHANGE_SUGGEST_STATUS);
-        }
-
-        if (negotiation.getUser().getId() == user.getId()) {
-            if (hasSuggestedPrice(updateDto)) {
-                reviseSuggestedPrice(updateDto, negotiation);
-                return new NegotiationResponseDto(CHANGE_SUGGEST);
-            }
-
-            if (hasStatus(updateDto)) {
-                if (isStatusAccept(negotiation)) {
-                    changeProposalToAccept(item, negotiation, status);
-                    changeProposalToReject(itemId, negotiationId);
-                    return new NegotiationResponseDto(PURCHASE_CONFIRM);
-                }
-            }
-        }
-
-        // TODO: refactor
-        throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        return negotiations.map(NegotiationResponse::of);
     }
 
-    @Transactional
-    public void deleteNegotiation(Long itemId, Long negotiationId, Long userId) {
-        Item item = itemRepository.findById(itemId)
-                .orElseThrow(() -> new MarketAppException(NOT_FOUND_ITEM, NOT_FOUND_ITEM.getMessage()));
-
-        Negotiation negotiation = negotiationRepository.findById(negotiationId)
-                .orElseThrow(() -> new MarketAppException(NOT_FOUND_NEGOTIATION, NOT_FOUND_NEGOTIATION.getMessage()));
-
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-
-        if (negotiation.getUser().getId() != user.getId()) {
-            throw new MarketAppException(INVALID_WRITER, INVALID_WRITER.getMessage());
-        }
-
-        negotiationRepository.delete(negotiation);
-    }
-
-    private boolean isStatusAccept(Negotiation negotiation) {
-        return negotiation.getStatus().equals(NegotiationStatus.ACCEPT);
-    }
-
-    private boolean hasStatus(NegotiationUpdateRequestDto updateDto) {
-        return updateDto.getSuggestedPrice() == 0;
-    }
-
-    private boolean hasSuggestedPrice(NegotiationUpdateRequestDto updateDto) {
-        return updateDto.getSuggestedPrice() != 0 && updateDto.getStatus() == null;
-    }
-
-    private void changeProposalToAccept(Item item, Negotiation negotiation, NegotiationStatus status) {
-        negotiation.updateNegotiationStatus(status);
-        item.updateStatus(ItemStatus.SOLD);
-    }
-
-    private int changeProposalToReject(Long itemId, Long negotiationId) {
-        return negotiationRepository.updateNegotiationStatus(negotiationId, itemId);
-    }
-
-    private void reviseSuggestedPrice(NegotiationUpdateRequestDto updateDto, Negotiation negotiation) {
-        negotiation.updateNegotiation(updateDto.getSuggestedPrice());
-    }
-
-    private Page<NegotiationListResponseDto> getNegotiationListResponseDtoBySeller(Long itemId, Pageable pageable) {
-        Page<Negotiation> allByItemId = negotiationRepository.findAllByItemId(itemId, pageable);
-
-        Page<NegotiationListResponseDto> listResponseDto = allByItemId.map(NegotiationListResponseDto::new);
-
-        return listResponseDto;
-    }
-
-    private Page<NegotiationListResponseDto> getNegotiationListResponseDtoByBuyer(Long itemId, Long userId, Pageable pageable) {
-        Page<Negotiation> allByItemIdAndUserId = negotiationRepository.findAllByItemIdAndUserId(itemId, userId, pageable);
-
-        Page<NegotiationListResponseDto> listResponseDto = allByItemIdAndUserId.map(NegotiationListResponseDto::new);
-
-        return listResponseDto;
-    }
+//    @Transactional
+//    public NegotiationResponseDto updateNegotiation(Long itemId, Long negotiationId, NegotiationUpdateRequestDto updateDto, Long userId) {
+//        Item item = itemRepository.findById(itemId)
+//                .orElseThrow(() -> new MarketAppException(NOT_FOUND_ITEM, NOT_FOUND_ITEM.getMessage()));
+//
+//        Negotiation negotiation = negotiationRepository.findById(negotiationId)
+//                .orElseThrow(() -> new MarketAppException(NOT_FOUND_NEGOTIATION, NOT_FOUND_NEGOTIATION.getMessage()));
+//
+//        User user = userRepository.findById(userId)
+//                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+//
+//        throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+//    }
 }
