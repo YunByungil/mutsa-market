357e928b9df40b53fe70106e81c5aa5c78d9d6e7
YunByungil <dbsquddlfz@naver.com>
2023-11-07 18:16:48 +0900

    :sparkles: TestSupport에 CommentController 추가

--- src/test/java/com/example/market/ControllerTestSupport.java
+++ src/test/java/com/example/market/ControllerTestSupport.java
@@ -1,17 +1,16 @@
 package com.example.market;
 
-import com.example.market.controller.CommentController;
-import com.example.market.controller.user.UserController;
+import com.example.market.api.controller.comment.CommentController;
+import com.example.market.api.controller.item.ItemController;
+import com.example.market.api.controller.user.UserController;
 import com.example.market.service.CommentService;
+import com.example.market.service.ItemService;
 import com.example.market.service.user.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.security.access.SecurityConfig;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -25,7 +24,8 @@ import java.util.List;
 //})
 @WebMvcTest(controllers = {
         UserController.class,
-        CommentController.class
+        CommentController.class,
+        ItemController.class
 }/*,
 excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)*/)
 public abstract class ControllerTestSupport {
@@ -41,6 +41,9 @@ public abstract class ControllerTestSupport {
 
     @MockBean
     protected CommentService commentService;
+
+    @MockBean
+    protected ItemService itemService;
 
     @BeforeEach
     void setUp() {
