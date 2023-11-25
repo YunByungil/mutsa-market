7aaa97cc8a6344a1917e4c6d9b11a29d6ebb700c
YunByungil <dbsquddlfz@naver.com>
2023-11-25 20:08:30 +0900

    :sparkles: 좌표값이 제대로 되어있는지에 대한 검증 메서드 추가

--- src/main/java/com/example/market/service/user/UserService.java
+++ src/main/java/com/example/market/service/user/UserService.java
@@ -1,5 +1,6 @@
 package com.example.market.service.user;
 
+import com.example.market.domain.entity.user.Coordinate;
 import com.example.market.domain.entity.user.User;
 import com.example.market.dto.user.request.UserCreateRequestDto;
 import com.example.market.dto.user.response.UserCreateResponseDto;
@@ -8,6 +9,7 @@ import com.example.market.exception.ErrorCode;
 import com.example.market.exception.MarketAppException;
 import com.example.market.repository.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.locationtech.jts.geom.Point;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,6 +27,8 @@ public class UserService {
     @Transactional
     public UserResponse createUser(UserCreateRequestDto dto) {
         validateDuplicateUsername(dto.getUsername());
+        validateLatAndLng(dto.getCoordinate());
+
         User user = dto.toEntity(passwordEncoder.encode(dto.getPassword()));
         User savedUser = userRepository.save(user);
 
@@ -36,5 +40,11 @@ public class UserService {
                 .ifPresent(user -> {
                     throw new MarketAppException(ALREADY_USER_USERNAME, ALREADY_USER_USERNAME.getMessage());
                 });
+    }
+
+    private void validateLatAndLng(final Coordinate coordinate) {
+        if (coordinate.getLat() == null || coordinate.getLng() == null) {
+            throw new MarketAppException(NOT_FOUND_COORDINATE, NOT_FOUND_COORDINATE.getMessage());
+        }
     }
 }
