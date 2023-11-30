848a0efb8b0b20c57a83465732ff5f676efe08b6
YunByungil <dbsquddlfz@naver.com>
2023-12-01 00:37:34 +0900

    :recycle: ItemStatus 예약중 추가 - 예약 중은 판매 중인 상품

--- src/main/java/com/example/market/domain/item/ItemStatus.java
+++ src/main/java/com/example/market/domain/item/ItemStatus.java
@@ -3,13 +3,16 @@ package com.example.market.domain.item;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Getter
 public enum ItemStatus {
-    SALE("판매중"), SOLD("판매완료");
+    SALE("판매중"), RESERVATION("예약중"), SOLD("판매완료");
+
     private final String status;
 
-    public static ItemStatus forDisplay() {
-        return SALE;
+    public static List<ItemStatus> forDisplay() {
+        return List.of(SALE, RESERVATION);
     }
 }
